732e5038ec53f4ffb32ddeff065d15fdbaba6dd1
Moritz Bischof <moritz.bischof@aleph-alpha.de>
2020-07-05 17:36:07 +0200

    v0.0.4

--- src/auth.rs
+++ src/auth.rs
@@ -1,14 +1,11 @@
+use crate::Result;
 use crate::ENDPOINT;
-use crate::{Error, Result};
-use parking_lot::Mutex;
 use reqwest::header::{ACCEPT, ACCEPT_LANGUAGE};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use std::ops::Deref;
 use std::sync::Arc;
 use std::time::Duration;
-use tokio::sync::oneshot;
-use tokio::task::JoinHandle;
+use tokio::stream::Stream;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct AccessToken {
@@ -17,77 +14,32 @@ pub struct AccessToken {
     pub expires_in: u64,
 }
 
-pub struct RefreshingAccessToken {
-    token: Arc<Mutex<AccessToken>>,
-    pub join_handle: JoinHandle<Result<()>>,
-    pub shutdown: oneshot::Sender<()>,
-}
-
-fn refresh_in(secs: u64) -> Duration {
-    Duration::from_secs(secs.saturating_sub(10))
-}
-
-async fn refresh_periodically(
-    token: Arc<Mutex<AccessToken>>,
-    client: Client,
-    id: String,
-    secret: String,
-) -> Error {
-    let mut timeout = refresh_in(token.lock().expires_in);
-    loop {
-        info!("refreshing access token in {:?}", timeout);
-        tokio::time::delay_for(timeout).await;
-
-        *token.lock() = match AccessToken::get(&client, &id, &secret).await {
-            Ok(token) => {
-                timeout = refresh_in(token.expires_in);
-                token
-            }
-            Err(err) => return err,
-        };
-    }
-}
-
-impl RefreshingAccessToken {
-    pub async fn refreshing(
+impl AccessToken {
+    pub fn refresh_periodically(
         client: &Client,
         id: impl Into<String>,
         secret: impl Into<String>,
-    ) -> Result<Self> {
-        let id = id.into();
-        let secret = secret.into();
+    ) -> impl Stream<Item = Result<AccessToken>> {
+        let credentials = Arc::new((id.into(), secret.into()));
+        let client = client.clone();
 
-        let initial = Arc::new(Mutex::new(AccessToken::get(client, &id, &secret).await?));
-        let refresh_future = refresh_periodically(initial.clone(), client.clone(), id, secret);
+        futures::stream::try_unfold(0, move |delay| {
+            let credentials = credentials.clone();
+            let client = client.clone();
+            async move {
+                if delay > 0 {
+                    tokio::time::delay_for(Duration::from_secs(delay)).await;
+                }
 
-        let (shutdown_tx, shutdown_rx) = oneshot::channel();
-        let join_handle = tokio::task::spawn(async move {
-            tokio::select! {
-                _ = shutdown_rx =>  {
-                    info!("stopping to refresh token");
-                    Ok(())
-                }
-                other = refresh_future => {
-                    error!("refreshing token failed: {}", other);
-                    Err(other)
-                }
+                let (id, secret) = &*credentials;
+                let token = AccessToken::acquire(&client, &id, &secret).await?;
+                let expires_in = token.expires_in;
+                Ok(Some((token, expires_in)))
             }
-        });
-
-        Ok(RefreshingAccessToken {
-            token: initial,
-            join_handle,
-            shutdown: shutdown_tx,
         })
     }
 
-    pub fn current(&self) -> AccessToken {
-        self.token.deref().lock().clone()
-    }
-}
-
-impl AccessToken {
-    pub async fn get(client: &Client, id: &str, secret: &str) -> Result<AccessToken> {
+    pub async fn acquire(client: &Client, id: &str, secret: &str) -> Result<AccessToken> {
         info!("acquiring access token..");
         let url = format!("{}/v1/oauth2/token?grant_type=client_credentials", ENDPOINT);
 
